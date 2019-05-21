eedf34bafeb16e1e5e62ba2fcb75cc5bd88a0442
frb92 <bartek1424@op.pl>
2019-05-21 20:52:09 +0200

    z laptopa

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Siema po raz drugi");
         System.out.println("A teraz trzeci?");
+        System.out.println("I z laptopa na PCta");
     }
 }
