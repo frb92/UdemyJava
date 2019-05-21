90d7e50e83b204fd6475d831532729eec50d148a
frb92 <bartek1424@op.pl>
2019-05-21 23:18:00 +0200

    'UdemyJavaIO'

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,60 @@
-public class Main {
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Siema po raz drugi");
-        System.out.println("A teraz trzeci?");
-        System.out.println("I z laptopa na PCta");
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        try {
+            File katalog = new File("Wideokursy" + File.separator + "php" + File.separator + "mysql");
+            katalog.mkdirs();
+            File plik2 = new File("Wideokursy" + File.separator + "php" + File.separator + "mysql", "lekcja1.txt");
+            if(!plik2.exists())
+                plik2.createNewFile();
+
+
+        File plik = new File("test.txt");
+        if(!plik.exists())
+            {
+            plik.createNewFile();
+            }
+        System.out.println("Czy moge pisac? "+plik.canWrite());
+        System.out.println("Czy moge odpalic? "+plik.canExecute());
+        System.out.println("Czy moge czytac? "+plik.canRead());
+        System.out.println("Czy jest ukryty? "+plik.isHidden());
+        System.out.println("Czy jest plikiem? "+plik.isFile());
+        System.out.println("Kiedy edytowane? "+new Date(plik.lastModified()));
+        System.out.println("Długośc pliku w bajtach: "+plik.length());
+
+        Main.wypiszSciezki(new File(System.getProperty("user.dir")));
+            }
+        catch (IOException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+
+        System.out.println(System.getProperty("java.home"));
+
+
+    }
+    public static void wypiszSciezki(File nazwaSciezki)
+    {
+
+        String[] nazwyPlikowIKatalogow = nazwaSciezki.list();
+
+
+
+        for(int i = 0; i < nazwyPlikowIKatalogow.length; i++)
+        {
+        File p = new File(nazwaSciezki.getPath(), nazwyPlikowIKatalogow[i]);
+
+        if(p.isFile())
+        System.out.println(p.getPath());
+
+        if (p.isDirectory())
+            wypiszSciezki(new File(p.getPath()));
+        }
     }
 }
