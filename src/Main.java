f641947b327d0d3a1de041c6a9bec9dadaa59f78
frb92 <bartek1424@op.pl>
2019-05-21 20:25:44 +0200

    'Nie'

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Siema po raz drugi");
+        System.out.println("A teraz trzeci?");
+    }
+}
